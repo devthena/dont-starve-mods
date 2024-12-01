ec09a5941422a4a435b0e4540c872a1b01c3e9a1
Athena <athena@parthenon.app>
2024-12-01 05:05:15 -0800

    chore(mod): Updated the mod version

--- chesters-for-everyone/modinfo.lua
+++ chesters-for-everyone/modinfo.lua
@@ -1,13 +1,13 @@
 name = "Chesters for Everyone"
 description = [[
-v0.2.0
+v0.2.1
 
 When a player joins the server, a personal Eye Bone will spawn next to them -- giving the player their very own Chester!
 
 The original Chester will still be in the world and won't be tied to the player.
 ]]
 author = "Athena (devthena)"
-version = "0.2.0"
+version = "0.2.1"
 api_version = 10
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"

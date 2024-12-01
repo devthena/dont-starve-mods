39e22c398c735d2a5f26eb336521a06ac4ee013b
Athena <athena@parthenon.app>
2024-12-01 01:59:33 -0800

    Initial Commit

---
+++ chesters-for-everyone/modinfo.lua
@@ -0,0 +1,7 @@
+name = "Chesters for Everyone"
+description = "Spawns a personal Eye Bone (Chester) next to players when they join."
+author = "Athena (devthena)"
+version = "0.1.0"
+api_version = 10
+dst_compatible = true
+server_only = true
---
+++ chesters-for-everyone/modmain.lua
@@ -0,0 +1,143 @@
+-- Import Declarations
+local require = GLOBAL.require
+local json = require "libs.json"
+
+-- Constants Declarations
+local DATA_DIR = "data"
+local EYEBONE_DATA_FILE = DATA_DIR .. "/eyebone_data.lua"
+local PLAYER_DATA_FILE = DATA_DIR .. "/player_data.lua"
+local SAVE_INTERVAL = 600 -- 10 minutes
+
+-- Variable Declarations
+local player_data = {}
+local eyebone_data = {}
+
+-- Creates a data directory if it does not exist
+local function CreateSaveDirectory()
+  if not TheSim:GetFilePath(DATA_DIR) then
+    TheSim:CreateDirectory(DATA_DIR)
+  end
+end
+
+-- Saves player data
+local function SavePlayerData()
+  local file = io.open(PLAYER_DATA_FILE, "w")
+
+  if file then
+    local json_data = json.encode(player_data)
+    file:write(json_data)
+    file:close()
+  end
+end
+
+-- Saves eyebone data
+local function SaveEyeBoneData()
+  local file = io.open(EYEBONE_DATA_FILE, "w")
+
+  if file then
+    for player_id, eyebone in pairs(player_data) do
+      local chester = eyebone.chester
+       eyebone_data[player_id] = {
+        eyebone_location = eyebone:GetPosition(),
+        chester_location = chester:GetPosition(),
+        chester_inventory = chester.components.inventory:GetItems(),
+      }
+    end
+
+    local json_data = json.encode(eyebone_data)
+    file:write(json_data)
+    file:close()
+  end
+end
+
+-- Saves eyebone data periodically
+local function PeriodicSave()
+  SaveEyeBoneData()
+end
+
+-- Loads player data
+local function LoadPlayerData()
+  local file = io.open(PLAYER_DATA_FILE, "r")
+  if file then
+    local json_data = file:read("*a")
+    player_data = json.decode(json_data)
+    file:close()
+  end
+end
+
+-- Loads eyebone data
+local function LoadEyeBoneData()
+  local file = io.open(EYEBONE_DATA_FILE, "r")
+  if file then
+    local json_data = file:read("*a")
+    eyebone_data = json.decode(json_data)
+    file:close()
+  end
+end
+
+-- Spawns an Eye Bone for existing players
+local function SpawnEyeBone(player, eyebone_state)
+  local player_name = player:GetDisplayName()
+  local eyebone = TheWorld:SpawnPrefab("eyebone")
+
+  local eyebone_pos = eyebone_state.eyebone_location
+  local chester_pos = eyebone_state.chester_location
+  local chester_inv = eyebone_state.chester_inventory
+
+  eyebone.Transform:SetPosition(eyebone_pos.x, eyebone_pos.y, eyebone_pos.z)
+  eyebone.components.named:SetCustomName(player_name .. "'s Eyebone")
+
+  local chester = eyebone.chester
+  
+  chester.Transform:SetPosition(chester_pos.x, chester_pos.y, chester_pos.z)
+
+  for _, item in ipairs(chester_inv) do
+    chester.components.inventory:GiveItem(item)
+  end
+
+  player_data[player.userid] = eyebone
+  SavePlayerData()
+end
+
+-- Spawns an Eye Bone for new players
+local function SpawnNewEyeBone(player)
+  local player_name = player:GetDisplayName()
+  local eyebone = TheWorld:SpawnPrefab("eyebone")
+  local x, y, z = player.Transform:GetWorldPosition()
+
+  eyebone.Transform:SetPosition(x + 2, y, z)
+  eyebone.components.named:SetCustomName(player_name .. "'s Eyebone")
+
+  player_data[player.userid] = eyebone
+  SavePlayerData()
+end
+
+-- Handler for Player Disconnect event
+local function OnPlayerDisconnect(player)
+  SaveEyeBoneData()
+end
+
+-- Handler for Player Join event
+local function OnPlayerJoin(player)
+  local eyebone_state = eyebone_data[player.userid]
+
+  if player_eyebone then
+    SpawnEyeBone(player, eyebone_state);
+    return
+  end
+
+  SpawnNewEyeBone(player)
+end
+
+CreateSaveDirectory()
+LoadPlayerData()
+LoadEyeBoneData()
+
+-- Listens for join and disconnect events
+AddPlayerPostInit(function(player)
+  player:ListenForEvent("on_disconnect", OnPlayerDisconnect)
+  player:ListenForEvent("on_join", OnPlayerJoin)
+end)
+
+-- Starts a periodic save of eyebone data
+TheWorld:DoTaskInTime(SAVE_INTERVAL, PeriodicSave)

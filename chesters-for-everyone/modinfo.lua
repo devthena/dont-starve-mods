8b00c98c71efead9a31aaedc57327f061b51e9fb
Athena <athena@parthenon.app>
2024-12-03 00:02:47 -0800

    chore(info): Added some properties for the modinfo.lua file

--- chesters-for-everyone/modinfo.lua
+++ chesters-for-everyone/modinfo.lua
@@ -1,15 +1,18 @@
 name = "Chesters for Everyone"
 description = [[
-Alpha v0.4.0
+Alpha v0.4.1
 
 When a player joins the server, a personal Eye Bone will spawn next to them -- giving the player their very own Chester!
 
 The original Chester will still spawn in the world and won't be tied to the player.
 ]]
 author = "Athena"
-version = "0.4.0"
+version = "0.4.1"
 api_version = 10
-icon_atlas = "modicon.xml"
-icon = "modicon.tex"
 dst_compatible = true
 all_clients_require_mod = true
+client_only_mod = false
+server_only_mod = false
+forumthread = ""
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
